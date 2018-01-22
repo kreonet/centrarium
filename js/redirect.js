6176f39bc97d444c29865067ef1ec66c1c48ac24
msjang <msjang@kisti.re.kr>
2018-01-22 20:38:02 +0900

    Add ko-en language change button

--- js/redirect.js
+++ js/redirect.js
@@ -4,3 +4,14 @@ SECOND_DOMAIN="eduroam.kreonet.net";
 if(document.location.href.includes(SECOND_DOMAIN)){
   document.location.href = document.location.href.replace(SECOND_DOMAIN,BASE_DOMAIN);
 }
+
+BASE_DIR="";
+
+function change_lang(){
+  ORIGIN = document.location.hostname + (document.location.port ? ':' + document.location.port: '') + BASE_DIR;
+  if(document.location.href.includes(ORIGIN+"/en")){
+    document.location.href = document.location.href.replace(ORIGIN+"/en",ORIGIN);
+  }else{
+    document.location.href = document.location.href.replace(ORIGIN,ORIGIN+"/en");
+  }
+}
